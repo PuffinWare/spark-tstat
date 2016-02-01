8bc098f4c4a381b0cedc1794b1c6def30aa37148
John Sokel <jsokel@signal.co>
2016-01-31 19:37:05 -0600

    Use new buttons, layout screen, general cleanup

--- application.cpp
+++ application.cpp
@@ -4,229 +4,187 @@ SYSTEM_MODE(MANUAL);
 
 #include "rgbled.h"
 
+#include "common.h"
 #include "RHT03.h"
-#include "TMP3x.h"
+#include "ButtonInterrupt.h"
 #include "OledDisplay.h"
 #include "font_lcd6x8.h"
 #include "font_lcd11x16.h"
-#include "font_12x16_bold.h"
-#include "common.h"
 
 // function prototypes
 void drawPattern();
 void drawText();
 void drawTemp();
+void drawSetTemp();
 void getTemp();
+void handleButtonHome(int mode);
+void handleButtonUp(int mode);
+void handleButtonDn(int mode);
 
-int btnUp = D6;
-int btnDn = D5;
-int btnHome = D3;
+#define DRAW_CUR_TEMP 1
+#define DRAW_SET_TEMP 1
+#define DRAW_MENU 2
 
-bool btnUpLatch = false;
-bool btnDnLatch = false;
-bool btnHomeLatch = false;
+#define PUFFIN_DEBUG true
 
-double avgTemp = 0.0;
+bool btnHomeToggle = false;
 
-uint drawMode = 0;
+int curTemp = 0;
+int curRH = 0;
+int setTemp = 72;
+int setRH = 30;
+
+uint drawMode = DRAW_CUR_TEMP;
 uint textMode = 0;
 
-OledDisplay display = OledDisplay(D1, D2, D0);;
-RHT03 rht = RHT03(D4, D7);
-TMP3x tmp36 = TMP3x(A0, 10, 1000);
+OledDisplay *display;
+RHT03 *rht;
+ButtonInterrupt  *btnHome;
+ButtonInterrupt  *btnUp;
+ButtonInterrupt  *btnDn;
 
 const font_t* font_lcdSm = parseFont(FONT_LCD6X8);
 const font_t* font_lcdLg = parseFont(FONT_LCD11X16);
-const font_t* font_bold  = parseFont(FONT_12X16_BOLD);
 
 void setup() {
+#ifdef PUFFIN_DEBUG
+  Serial.begin(9600);
+#endif
 
-  pinMode(btnUp, INPUT_PULLUP);
-  pinMode(btnDn, INPUT_PULLUP);
-  pinMode(btnHome, INPUT_PULLUP);
-
-  // Spark.variable("reading", &reading, DOUBLE);
-  // Spark.variable("volts", &volts, DOUBLE);
-  // Spark.variable("temp", &avgTemp, DOUBLE);
+  pinMode(D7, OUTPUT);
+  digitalWrite(D7, LOW);
 
   RGB.control(true);
   RGB.color(0, 0, 0);
 
-  display.begin();
+  display = new OledDisplay(D1, D2, D0);
+  display->begin();
+
+  rht = new RHT03(D3);
+
+  btnHome = new ButtonInterrupt(D4, 100, &handleButtonHome, 2000, 250);
+  btnUp = new ButtonInterrupt(D6, 100, &handleButtonUp, 2000, 250);
+  btnDn = new ButtonInterrupt(D5, 100, &handleButtonDn, 2000, 250);
 }
 
 void loop() {
-  tmp36.poll();
-  if (rht.poll()) {
-    drawTemp();
-  }
-
-  // curTime = millis();
-  // // get temp once per second
-  // if (lastTime + 3000 < curTime) {
-  //   lastTime = curTime;
-  //   display.clear(CLEAR_OLED);
-  //   drawTemp();
-  // }
-
-  if (digitalRead(btnHome) == LOW) {
-    if (btnHomeLatch) { // only draw once per press
-      drawPattern();
-      btnHomeLatch = false;
-    }
-  } else {
-    btnHomeLatch = true;
-  }
-
-  if (digitalRead(btnUp) == LOW) {
-    RGB.color(0,0,255);
-    if (btnUpLatch) { // only draw once per press
+  if (rht->poll()) {
+    getTemp();
+    if (drawMode == DRAW_CUR_TEMP) {
       drawTemp();
-      btnUpLatch = false;
     }
-  } else {
-    btnUpLatch = true;
   }
 
-  if (digitalRead(btnDn) == LOW) {
-    RGB.color(255,0,0);
-    if (btnDnLatch) { // only draw once per press
-      drawText();
-      btnDnLatch = false;
-    }
-  } else {
-    btnDnLatch = true;
-  }
+  btnHome->poll();
+  btnUp->poll();
+  btnDn->poll();
 }
 
 void initStat() {
 }
 
+void handleButtonHome(int mode) {
+  switch(mode) {
+    case UP:
+      digitalWrite(D7, LOW);
+      break;
+
+    case FIRST:
+      digitalWrite(D7, HIGH);
+      btnHomeToggle = true;
+      break;
+
+    case REPEAT:
+      digitalWrite(D7, btnHomeToggle ? HIGH : LOW);
+      btnHomeToggle = !btnHomeToggle;
+      break;
+
+    default:
+      break;
+  }
+}
+
+void handleButtonUp(int mode) {
+  switch(mode) {
+    case FIRST:
+    case REPEAT:
+      setTemp++;
+      drawSetTemp();
+      break;
+
+    default:
+      break;
+  }
+}
+
+void handleButtonDn(int mode) {
+  switch(mode) {
+    case FIRST:
+    case REPEAT:
+      setTemp--;
+      drawSetTemp();
+      break;
+
+    default:
+      break;
+  }
+}
+
+void getTemp() {
+  curTemp = rht->getTempF();
+  curRH = rht->getRH();
+  // TODO: Send to server
+}
+
 void drawTemp() {
-  display.clear(CLEAR_OLED);
-  display.setFont(font_lcdLg);
+  // todo: do this only once
+  display->clear(CLEAR_BUFF);
+
+  display->setFont(font_lcdLg);
   char tempStr[8];
 
-  double ftemp = tmp36.getTempF();
-  ftemp = (ftemp / 10);
-
-  sprintf(tempStr, "%.1f\x7f", ftemp);
-  display.writeText(0, 0, tempStr);
-
-  ftemp = rht.getTempF();
+  int ftemp = curTemp;
+  int fdec = ftemp % 10;
   ftemp = ftemp / 10;
-  sprintf(tempStr, "%.1f\x7f", ftemp);
-  display.writeText(0, 1, tempStr);
 
-  ftemp = rht.getRH();
-  ftemp = ftemp / 10;
-  sprintf(tempStr, "%.1f%%", ftemp);
-  display.writeText(0, 2, tempStr);
+  display->setFont(font_lcdLg);
+  sprintf(tempStr, "%d.", ftemp);
+  display->writeText(0, 0, tempStr);
+  display->setFont(font_lcdSm);
+  sprintf(tempStr, "%d", fdec);
+  display->writeText(5, 1, tempStr);
 
-  // sprintf(tempStr, "%d.%d  ", rht.getIntCount(), rht.getIgnCount());
-  // display.writeText(1, 4, tempStr);
-  display.display();
+  int rh = curRH;
+  int rhdec = rh % 10;
+  rh = rh / 10;
+  display->setFont(font_lcdLg);
+  sprintf(tempStr, "%d.", rh);
+  display->writeText(0, 1, tempStr);
+  display->setFont(font_lcdSm);
+  sprintf(tempStr, "%d%%", rhdec);
+  display->writeText(5, 3, tempStr);
+
+  display->setFont(font_lcdSm);
+  const char *curDate = "10/14";
+  const char *curTime = "12:34";
+  display->writeText(0, 5, curDate);
+  display->writeText(6, 5, curTime, -3);
+
+  sprintf(tempStr, "%d", setTemp);
+  display->writeText(8, 0, tempStr, 4);
+  sprintf(tempStr, "%d", setRH);
+  display->writeText(8, 2, tempStr, 4);
+
+  display->display();
 }
 
-void drawPattern() {
-  display.resetPage();
+void drawSetTemp() {
+  char tempStr[8];
 
-  switch (drawMode) {
-    case 0: // blank page
-      RGB.color(255,0,0);
-      display.clear(CLEAR_BUFF);
-      break;
-    case 1: // horizontal lines
-      RGB.color(0,255,0);
-      display.fill(0xaa);
-      break;
-    case 2: // vertical lines
-      RGB.color(0,0,255);
-      for (int i=0; i<6; i++) {
-        for (int j=0; j<32; j++) {
-          int c = j*2;
-          display.setByte(i, c, 0xff);
-          display.setByte(i, c+1, 0x00);
-        }
-      }
-      break;
-    case 3: {// count
-      RGB.color(255,255,0);
-      byte val = 0x00;
-      for (int i=0; i<6; i++) {
-        for (int j=0; j<64; j++) {
-          display.setByte(i, j, val++);
-          if (val > 0xff) val = 0x00;
-        }
-      }
-      break;
-    }
-    case 4: // diagnal lines
-      RGB.color(255,0,255);
-      for (int i=0; i<6; i++) {
-        for (int j=0; j<8; j++) {
-          int c = j*8;
-          display.setByte(i, c,   0x01);
-          display.setByte(i, c+1, 0x02);
-          display.setByte(i, c+2, 0x04);
-          display.setByte(i, c+3, 0x08);
-          display.setByte(i, c+4, 0x10);
-          display.setByte(i, c+5, 0x20);
-          display.setByte(i, c+6, 0x40);
-          display.setByte(i, c+7, 0x80);
-        }
-      }
-      break;
-    case 5: // progression
-      RGB.color(0,255,255);
-      for (int i=0; i<6; i++) {
-        for (int j=0; j<8; j++) {
-          int c = j*8;
-          display.setByte(i, c,   0x01);
-          display.setByte(i, c+1, 0x03);
-          display.setByte(i, c+2, 0x07);
-          display.setByte(i, c+3, 0x0f);
-          display.setByte(i, c+4, 0x1f);
-          display.setByte(i, c+5, 0x3f);
-          display.setByte(i, c+6, 0x7f);
-          display.setByte(i, c+7, 0xff);
-        }
-      }
-      break;
-  }
-  display.display();
+  sprintf(tempStr, "%d", setTemp);
+  display->writeText(8, 0, tempStr, 4);
+  sprintf(tempStr, "%d", setRH);
+  display->writeText(8, 2, tempStr, 4);
 
-  drawMode += 1;
-  if (drawMode > 5) {
-      drawMode = 0;
-  }
-}
-
-void drawText() {
-  display.clear(CLEAR_OLED);
-  // display.setFont(textMode);
-  switch (textMode) {
-    case 0: // small
-    case 1: // med
-    case 2: // bold
-      // RGB.color(textMode==0?255:0, textMode==0?0:255, 0);
-      display.writeText(0, 0, "12345");
-      display.writeText(0, 1, "Hello");
-      display.writeText(0, 2, "World");
-      break;
-    case 3: // large
-    case 4:
-    case 5:
-      // RGB.color(0,0,255);
-      display.writeText(0, 0, "12345");
-      if (textMode < 5) {
-        display.writeText(0, 1, "67890");
-      }
-      break;
-  }
-
-  textMode += 1;
-  if (textMode > 5) {
-    textMode = 0;
-  }
+  display->display();
 }
